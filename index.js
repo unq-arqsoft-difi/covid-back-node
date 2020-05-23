e0b281fd9eb8976cef3150c2d111a4cb41764b9b
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2020-05-23 19:35:51 -0300

    :lipstick: change routes printer

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const morgan      = require('morgan');
 const logger      = require('winston-ready');
+const routeList   = require('express-routes-catalogue');
 const app         = require('./src/server');
-const printRoutes = require('./src/lib/console-routes');
 
 const PORT = process.env.SERVER_PORT || 9004;
 
 // Middleware for express
 app.use(morgan('common', { stream: { write: message => logger.verbose(message) } }));
 
-
 app.listen(PORT, () => {
   logger.info('Server running on port %d', PORT);
   if (process.env.NODE_ENV === 'development') {
-    printRoutes(app);
+    routeList.default.terminal(app);
   }
 });
