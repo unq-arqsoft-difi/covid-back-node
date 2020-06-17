d1d1fa3d720c6faf24b102aefaf5f9fa406b7897
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2020-06-16 21:32:20 -0300

    :bug: fix listen problem

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const { printRoutes } = require('@leandrojdl/express-routes');
 const app             = require('./src/server');
 
 const port = process.env.SERVER_PORT || 9004;
-const domain = process.env.SERVER_DOMAIN || 'localhost';
 
-app.listen(port, domain, () => {
-  console.info(`Server running on http://${domain}:${port}`);
+app.listen(port, () => {
+  console.info(`Server running on ${port}`);
   if (process.env.NODE_ENV === 'development') printRoutes(app);
 });
