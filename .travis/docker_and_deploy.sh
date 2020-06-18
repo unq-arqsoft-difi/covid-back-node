8a1ce2ed29a3d4e1204595d777689dac240f3bef
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2020-06-17 23:50:43 -0300

    :wrench::bug: fix CI/CD script

--- .travis/docker_and_deploy.sh
+++ .travis/docker_and_deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "${TRAVIS_BRANCH}" = "push-dockerhub" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+if [ "${TRAVIS_BRANCH}" = "master" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     echo "*** Building Docker Containers ***"
     docker build -f docker/Dockerfile -t unqdifi/covid-back-node .
 
