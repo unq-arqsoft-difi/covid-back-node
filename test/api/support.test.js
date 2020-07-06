cdc5a215744b0667f9ddb5ddb8eb6be85642f360
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2020-07-05 22:37:56 -0300

    :green_heart: add tests for institutions & supplies

--- test/api/support.test.js
+++ test/api/support.test.js
@@ -1,6 +1,12 @@
 const { OK, NOT_FOUND } = require('http-status-codes');
 const { api } = require('../test-case');
-const { Area, Province, Town } = require('../../db/models');
+const {
+  Area,
+  Institution,
+  Province,
+  Supply,
+  Town,
+} = require('../../db/models');
 
 describe('/support', () => {
   describe('/areas', () => {
@@ -45,7 +51,7 @@ describe('/support', () => {
       expect(province).toEqual({ id: '01', name: 'The North' });
     });
 
-    describe('/provinces/:id', () => {
+    describe('/:id', () => {
       test('Not Found', async () => {
         const { body, status } = await api.get('/support/provinces/01');
         expect(status).toBe(NOT_FOUND);
@@ -63,7 +69,7 @@ describe('/support', () => {
       });
     });
 
-    describe('/provinces/:id/towns', () => {
+    describe('/:id/towns', () => {
       test('Empty towns', async () => {
         await Province.create({ id: '01', name: 'The North' });
         const { body, status } = await api.get('/support/provinces/01/towns');
@@ -87,6 +93,82 @@ describe('/support', () => {
 
         const [town] = body.towns;
         expect(town).toEqual({ id: 'ABC', name: 'Winterfell' });
+      });
+    });
+  });
+
+  describe('/institutions', () => {
+    beforeEach(async () => {
+      await Town.sync({ force: true });
+      await Province.sync({ force: true });
+      await Institution.sync({ force: true });
+    });
+
+    test('Empty list', async () => {
+      const { body, status } = await api.get('/support/institutions');
+      expect(status).toBe(OK);
+      expect(body).toBeArrayOfSize(0);
+    });
+
+    test('With an Institution', async () => {
+      await Institution.create({
+        id: '01',
+        name: 'Citadel',
+        funding: 'Municipal',
+        town: {
+          id: '11',
+          name: 'OldTown',
+        },
+        province: {
+          id: '21',
+          name: 'Westeros',
+        },
+      }, {
+        include: [
+          { model: Town, as: 'town' },
+          { model: Province, as: 'province' },
+        ],
+      });
+
+      const { body, status } = await api.get('/support/institutions');
+      expect(status).toBe(OK);
+      expect(body).toBeArrayOfSize(1);
+
+      const [area] = body;
+      expect(area).toEqual({
+        id: '01',
+        name: 'Citadel',
+        funding: 'Municipal',
+        townId: '11',
+        provinceId: '21',
+      });
+    });
+  });
+
+  describe('/supplies', () => {
+    beforeEach(async () => Supply.sync({ force: true }));
+
+    test('Empty list', async () => {
+      const { body, status } = await api.get('/support/supplies');
+      expect(status).toBe(OK);
+      expect(body).toBeArrayOfSize(0);
+    });
+
+    test('With a Supply', async () => {
+      await Supply.create({
+        id: 1,
+        name: 'Gloves',
+        stock: 10000,
+      });
+      const { body, status } = await api.get('/support/supplies');
+      expect(status).toBe(OK);
+      expect(body).toBeArrayOfSize(1);
+
+      const [supply] = body;
+      expect(supply).toEqual({
+        id: 1,
+        name: 'Gloves',
+        stock: 10000,
       });
     });
   });
