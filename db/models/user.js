e0fc477d5617589190ac0c50595a5cecb27b3d28
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2020-07-10 17:13:26 -0300

    :recycle: refactor register user according to migration chganges

--- db/models/user.js
+++ db/models/user.js
@@ -1,13 +1,29 @@
+const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
+  class User extends Model {
+    static associate(models) {
+      User.belongsTo(models.Town, { as: 'town' });
+      User.belongsTo(models.Province, { as: 'province' });
+      User.belongsTo(models.Institution, { as: 'institution' });
+    }
+  }
+
+  User.init({
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
     email: DataTypes.STRING,
     phone: DataTypes.STRING,
-    entity: DataTypes.STRING,
     job: DataTypes.STRING,
-    place: DataTypes.STRING,
     pass: DataTypes.STRING,
-  }, {});
+    admin: DataTypes.BOOLEAN,
+  }, {
+    sequelize,
+    modelName: 'User',
+    defaultScope: {
+      attributes: { exclude: ['createdAt', 'updatedAt', 'pass'] },
+    },
+  });
+
   return User;
 };
