c60627717c9f429c3f646d8014bca63c1b8e76be
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2020-09-24 19:07:14 -0300

    :seedling: using local institutions seed

--- db/seeders/20200628001950-institutions.js
+++ db/seeders/20200628001950-institutions.js
@@ -1,50 +1,16 @@
-const axios = require('axios');
-const logger = require('../../src/lib/logger');
-const { Town } = require('../models');
+const institutions = require('./data-institutions.json');
 
-function stripBOM(content) {
-  if (content.charCodeAt(0) === 0xFEFF) {
-    return content.slice(1);
-  }
-  return content;
-}
-
-async function townId(institution) {
-  const town = await Town.findOne({
-    where: { name: institution.localidad_nombre },
-    attributes: ['id', 'name'],
-  });
-  return town ? town.id : null;
-}
-
-const url = 'http://datos.salud.gob.ar/dataset/336cf4d9-447a-44c4-8e34-0ba1fc293d55/resource/a7b106d5-effe-4c98-846a-ac81b8f48cb0/download/listado-establecimientos-salud-asentados-registro-federal-refes-201908.json';
-
-const fetchInstitutions = () => axios.get(url)
-  .then(async (res) => {
-    const data = stripBOM(res.data); // algún forro retorna UTF-8 with BOM
-    const institutions = JSON.parse(data);
-    const mapped = await Promise.all(institutions.filter((item) => {
-      const isClinic = item.establecimiento_nombre.includes('CLINICA');
-      const isHospital = item.establecimiento_nombre.includes('HOSPITAL');
-      const isSanatorium = item.establecimiento_nombre.includes('SANATORIO');
-      return isClinic || isHospital || isSanatorium;
-    })
-      .map(async institution => ({
-        id: institution.establecimiento_id,
-        name: institution.establecimiento_nombre,
-        funding: institution.origen_financiamiento,
-        townId: await townId(institution), // algún otro forro no normalizó los IDs
-        provinceId: institution.provincia_id.padStart(2, '0'),
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      })));
-    return mapped.sort((a, b) => a.id.localeCompare(b.id));
-  }).catch((error) => {
-    logger.error(error);
-    throw new Error(error);
-  });
+const parseInstitutions = () => institutions.map(institution => ({
+  id: `${institution.id}`,
+  name: institution.name,
+  funding: institution.funding,
+  townId: `${institution.townId}`,
+  provinceId: `${institution.provinceId}`,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+}));
 
 module.exports = {
-  up: async queryInterface => queryInterface.bulkInsert('Institutions', await fetchInstitutions(), {}),
+  up: async queryInterface => queryInterface.bulkInsert('Institutions', parseInstitutions(), {}),
   down: queryInterface => queryInterface.bulkDelete('Institutions', null, {}),
 };
